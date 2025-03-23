959c543b14dfa7ba4e235e7a457fc1e488203482
peleghila <peleg.hila@gmail.com>
2025-03-23 19:28:59 +0200

    returning the right thing, too

--- Palamedes/Gen2.lean
+++ Palamedes/Gen2.lean
@@ -4,14 +4,16 @@ inductive Gen' : (α : Type) → (P: α → Prop) → Type 1 where
 | gt (lo : Int) (p : ∀ v, P v ↔ v > lo) : Gen' Int P
 | lt (hi : Int) (p : ∀ v, P v ↔ v < hi) : Gen' Int P
 | pick (g1 : Gen' α P₁) (g2 : Gen' α P₂) (p: ∀ v, P v ↔ P₁ v ∨ P₂ v) : Gen' α P
-| bind (g1 : Gen' α P₁) (f : α → Gen' β P₂)
-       (p: ∀ v, ∃ v', P₁ v' ∧ P₂ v ) : Gen' β P
+| bind {P₁} {P₂} (g1 : Gen' α P₁) (f : α → Gen' β P₂)
+       (p: ∀ v, ∃ v', P₁ v' ∧ P₂ v ) : Gen' β P₂
 
 
 def range : Gen' (Int × Int) (fun (v1,v2) => v1 = 0 ∧ v2 > v1) :=
-  Gen'.bind
-    (Gen'.gt 0)
-    (Gen'.ret (0,.))
+  -- Gen'.bind
+  --   (Gen'.gt 0 _)
+  --   (λ v => (Gen'.ret (0,v) _))
+  --   _
+  Gen'.bind (Gen'.gt 0 _) (λ x => (Gen'.ret (0, x) _)) _
 
 -- instance : Monad Gen' where
 --   pure := .ret
