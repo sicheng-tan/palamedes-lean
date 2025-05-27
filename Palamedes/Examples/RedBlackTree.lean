789cc5d0c680f06c4fa3a5c1484dc4f856cc31b1
cassiatorczon <cassiatorczon@gmail.com>
2025-05-27 00:03:08 -0400

    initial red-black tree def

---
+++ Palamedes/Examples/RedBlackTree.lean
@@ -0,0 +1,44 @@
+import Palamedes.Synth
+import Palamedes.Sample
+import Palamedes.Data.Tree
+import Mathlib.Tactic.Convert
+
+namespace RedBlackTree
+
+#set_up_palamedes_simp
+
+inductive Color where
+  | red
+  | black
+
+-- def RBT α := Tree (color × α)
+
+def rr_aux : Tree (Color × Nat) → Bool → Bool := λ t isRedChild =>
+ match t with
+ | .leaf => true
+ | .node l (.red, _) r => (not isRedChild) && rr_aux l true && rr_aux r true
+ | .node l (.black, _) r => rr_aux l false && rr_aux r false
+
+@[simp]
+def rr : Tree (Color × Nat) → Bool := λ t => rr_aux t false
+
+def bh : Tree (Color × Nat) → Nat → Bool := λ t height =>
+ match t with
+ | .leaf => height == 1
+ | .node l (.red, _) r => bh l height && bh r height
+ | .node l (.black, _) r =>
+    height > 0 &&
+    bh l (height - 1) &&
+    bh r (height - 1)
+
+def isBST : Tree (α × Nat) → (Nat × Nat) → Bool := λ t ⟨lo, hi⟩ =>
+  match t with
+  | .leaf => true
+  | .node l (_, x) r =>
+    (lo <= x && x <= hi) &&
+    isBST l ⟨lo, x - 1⟩ &&
+    isBST r ⟨x + 1, hi⟩
+
+def genRBT
+    (height lo hi : Nat) :
+    CGen (λ t => rr t ∧ bh t height ∧ isBST t (lo, hi)) := by sorry
