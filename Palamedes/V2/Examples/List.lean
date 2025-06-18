68aa4f340a2fdbcc7c2a01af10ee68423c484332
cassiatorczon <cassiatorczon@gmail.com>
2025-06-18 13:19:59 -0400

    inprogress proofs in List examples

--- Palamedes/V2/Examples/List.lean
+++ Palamedes/V2/Examples/List.lean
@@ -2,82 +2,109 @@ import Palamedes.V2.Synthesizer
 
 open Gen CorrectGen
 
--- def genAllTwosFold : Gen (List Nat) := by
---   generator_search fun xs => List.fold (fun x b => x == 2 && b) true xs = true
-
 -- def genTrueFold : Gen (List Nat) := by
 --   generator_search (fun (xs : List Nat) => List.fold (fun x b => b) true xs = true)
-
--- def genEvenLenFold : Gen (List Nat) := by
---   generator_search (fun (xs : List Nat) => List.fold (fun x b => !b) true xs = true)
 
 -- def genLengthKFold {k : Nat} : Gen (List Nat) := by
 --   generator_search (fun xs => List.fold (fun x b => b + 1) 0 xs = k)
 
--- def genEvenLenTwosFold : Gen (List Nat) := by
---   -- generator_search (fun xs => List.fold (fun x b => x == 2 && b) true xs = true ∧ List.fold (fun x b => !b) true xs = true)
---   let cg : CorrectGen (fun (xs : List Nat) => List.fold (fun x b => x == 2 && b) true xs = true ∧ List.fold (fun x b => !b) true xs = true) := by
---     apply convert (by
---       funext
---       simp [guard, *]
---       rw [List.fold_accu_Option_true] <;> try (aesop; done)
---       rw [List.fold_accu_Option_basic] <;> try (aesop; done)
---       rw [List.merge_accuM]
---     ) (List.cunfold _)
---     intros b s
---     replace ⟨ (), b ⟩ := b
---     apply caseBool (by assumption) <;> intro h
---     . gapply (cpick _ _)
---       . cgenerator_search
---       . gapply (cbind _ _) --?
---         . cgenerator_search
---         . intro x
---           replace ⟨ x, _ ⟩ := x
---           gapply (cbind _ _)
---           . cgenerator_search
---           . intro y
---             replace ⟨ (), _ ⟩ := y
---             generalize hbx : (x == 2) = bx
---             apply (caseBool (by assumption)) <;> intro h
---             . rw [h, Nat.beq_eq_true_eq] at hbx
---               cgenerator_search
---             . rw [h] at hbx
---               simp_all only [beq_eq_false_iff_ne, ne_eq, ite_false, Option.bind, Eq.symm, Option.some_ne_none]
---               -- cgenerator_search
---               --simp only [bind, failure, Option.bind]
---               sorry
---     . simp_all
---       gapply (cbind _ _)
---       . cgenerator_search
---       . intro y
---         replace ⟨ y, _ ⟩ := y
---         simp_all
---         generalize hb : (y == 2) = b
---         apply (caseBool (by assumption)) <;> intro h <;> simp_all
---         . cgenerator_search
---         . sorry
---   all_goals sorry
+-- def genAllTwosFold : Gen (List Nat) := by
+--   generator_search (fun xs => List.fold (fun x b => x == 2 && b) true xs = true)
 
+-- def genEvenLenFold : Gen (List Nat) := by
+--   generator_search (fun xs => List.fold (fun x b => !b) true xs = true)
 
-  -- let g : Gen (List Nat) := by
-  --   optimize_gen cg.val
-  -- let _ : support cg.val = support g := by
-  --   optimality
-  -- let _ : Gen.total g := by
-  --   totality
-  -- exact g
+set_option palamedes.debug true
+set_option pp.proofs true
 
--- def genLengthKTwosFold (k : Nat) : Gen (List Nat) := by
---   -- generator_search (fun (xs : List Nat) =>
---   --     List.fold (fun _ l => l + 1) 0 v = k ∧
---   --     List.fold (fun x () => guard (x == 2)) () v = Option.some ())
---   sorry
+def genEvenLenTwosFold : Gen (List Nat) := by
+  -- generator_search (fun xs => List.fold (fun x b => x == 2 && b) true xs = true ∧ List.fold (fun x b => !b) true xs = true)
+  let cg : CorrectGen (fun (xs : List Nat) => List.fold (fun x b => x == 2 && b) true xs = true ∧ List.fold (fun x b => !b) true xs = true) := by
+    apply convert (by
+      funext
+      simp [guard, *]
+      rw [List.fold_accu_Option_true] <;> try (aesop; done)
+      rw [List.fold_accu_Option_basic] <;> try (aesop; done)
+      rw [List.merge_accuM]
+    ) (List.cunfold _)
+    intros b s
+    replace ⟨ (), b ⟩ := b
+    -- simp only [Option.bind_eq_bind, Option.some_bind, Option.some.injEq,
+    --   Prod.mk.injEq, Option.bind_eq_some, Option.guard_eq_some, true_and]
+    apply caseBool (by assumption) <;> intro h <;> rw [h]
+    . gapply (cpick _ _)
+      . cgenerator_search
+      . apply convert (by
+          funext
+          -- TODO here
+          simp only [Option.bind_eq_bind, Option.some_bind, Option.some.injEq, Prod.mk.injEq,
+          Bool.true_eq_false, and_false, false_and, guard, beq_iff_eq, Option.pure_def,
+          Option.bind_eq_some, ite_eq_left_iff, reduceCtorEq, imp_false, Decidable.not_not,
+          Bool.not_eq_eq_eq_not, Bool.not_false, true_and, exists_const, Prod.exists,
+          Bool.exists_bool, Bool.false_eq_true, and_true, false_or, or_false, exists_and_left]
+          rfl
+          ) (cbind _ _)
+        . cgenerator_search
+        . cgenerator_search
+    . apply convert (by
+        funext
+        -- TODO here
+        simp only [Option.bind_eq_bind, Option.some_bind, Option.some.injEq, Prod.mk.injEq,
+        Bool.true_eq_false, and_false, false_and, guard, beq_iff_eq, Option.pure_def,
 
+        Option.bind_eq_some, ite_eq_left_iff, reduceCtorEq, imp_false, Decidable.not_not,
+        Bool.not_eq_eq_eq_not, Bool.not_false, true_and, exists_const,
+        Prod.exists, Bool.exists_bool,
+        Bool.false_eq_true, and_true, false_or, exists_and_left, eq_iff_iff]
+        rfl
+        ) (cbind _ _)
+      . cgenerator_search
+      . cgenerator_search
+  let g : Gen (List Nat) := by
+      optimize_gen cg.val
+  let _ : support cg.val = support g := by
+    sorry
+  let _ : Gen.total g := by
+    sorry
+  exact g
 
--- set_option palamedes.debug true
+def genLengthKTwosFold (k : Nat) : Gen (List Nat) := by
+  -- generator_search (fun (xs : List Nat) => List.fold (fun x l => l + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true)
+  let cg : CorrectGen (fun (xs : List Nat) => List.fold (fun x l => l + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true) := by
+    apply convert (by
+      funext
+      simp [guard, *]
+      rw [List.fold_accu_Option_basic] <;> try (aesop; done)
+      rw [List.fold_accu_Option_true] <;> try (aesop; done)
+      rw [List.merge_accuM]
+    ) (List.cunfold _)
+    intros n s
+    replace ⟨ n, () ⟩ := n
+    apply caseNat (by assumption)
+    . intro h
+      rw [h]
+      apply convert (by
+        funext
+        simp [guard, Option.bind_eq_some, *]
+        rfl
+        ) (cpure _)
+    . intro n' h
+      rw [h]
+      apply convert (by
+        funext
+        simp [guard, Option.bind_eq_some, *] -- TODO here
+        sorry) (cbind _ _)
+      sorry
+  let g : Gen (List Nat) := by
+    optimize_gen cg.val
+  let _ : support cg.val = support g := by
+    optimality
+  let _ : Gen.total g := by
+    totality
+  exact g
 
-def genIncreasingByOneFold : Gen (List Nat) := by
-  generator_search (fun xs => List.fold (fun x b prev => x == prev + 1 && b x) (fun x => true) xs 0 = true)
+-- def genIncreasingByOneFold : Gen (List Nat) := by
+--   generator_search (fun xs => List.fold (fun x b prev => x == prev + 1 && b x) (fun x => true) xs 0 = true)
 
 -- def genSortedBetween (lo hi : Nat) : Gen (List Nat) := by
 --   generator_search
@@ -98,25 +125,3 @@ def genIncreasingByOneFold : Gen (List Nat) := by
     --       | apply exists_congr; intro; rw [true_and]
     --       | rfl
     --   | rfl) (List.cunfold _)
-
--- set_option palamedes.debug true
-
--- add_aesop_rules unsafe (rule_sets := [synthesis]) [
---   (by fail_if_no_progress intros),
---   (by assumption),
---   (by gapply (cpure _)),
---   (by gapply (cpick _ _)),
---   (by gapply (cbind _ _)),
---   (by gapply (List.cunfold _)),
---   (by gapply carbUnit),
---   (by gapply carbBool),
---   (by gapply carbNat),
---   (by gapply cgt),
---   (by gapply cbetween_partial),
---   (by gapply (cbetween (by first | aesop | omega))),
--- ]
-
--- add_aesop_rules 5% (rule_sets := [synthesis]) [
---   (by apply caseBool (by assumption)),
---   (by apply caseNat (by assumption))
--- ]
