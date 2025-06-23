c94293bfa5012f362eea14ad8214ab112178d5a3
cassiatorczon <cassiatorczon@gmail.com>
2025-06-23 13:32:34 -0400

    stubbed a couple list  examples

--- Palamedes/Examples/List/AllTwos.lean
+++ Palamedes/Examples/List/AllTwos.lean
@@ -4,3 +4,8 @@ open Gen CorrectGen
 
 def genAllTwosFold : Gen (List Nat) := by
   generator_search (fun xs => List.fold (fun x b => x == 2 && b) true xs = true)
+
+set_option palamedes.debug true
+
+-- def genAllTwos : Gen (List Nat) := by
+--   generator_search (fun xs => List.all xs ( · = 2))
--- Palamedes/Examples/List/True.lean
+++ Palamedes/Examples/List/True.lean
@@ -4,3 +4,6 @@ open Gen CorrectGen
 
 def genTrueFold : Gen (List Nat) := by
   generator_search (fun (xs : List Nat) => List.fold (fun x b => b) true xs = true)
+
+-- def genTrue : Gen (List Nat) := by
+--   generator_search (fun _ => true)
