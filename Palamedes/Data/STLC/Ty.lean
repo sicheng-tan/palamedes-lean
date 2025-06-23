f79204296b8aa88f726dce6e4927a9b498ed2994
cassiatorczon <cassiatorczon@gmail.com>
2025-06-23 13:28:12 -0400

    ported STLC and stack

---
+++ Palamedes/Data/STLC/Ty.lean
@@ -0,0 +1,445 @@
+import Palamedes.Gen
+import Palamedes.CorrectGen
+import Palamedes.Total
+
+section TypeDef
+
+inductive Ty : Type where
+  | unit
+  | arrow (τ₁ τ₂ : Ty)
+  deriving DecidableEq, Repr
+
+end TypeDef
+
+section BaseFunctor
+
+inductive TyF (α : Type) where
+  | unit : TyF α
+  | arrow : (τ₁ : α) → (τ₂ : α) → TyF α
+
+theorem TyF_or
+    {α : Type}
+    {P : Prop}
+    {Q : α → α → Prop}
+    {τ : TyF α} :
+    TyF.rec P Q τ ↔ (P ∧ τ = .unit) ∨ (∃ b₁ b₂, τ = .arrow b₁ b₂ ∧ Q b₁ b₂) := by
+  match τ with
+  | .unit => simp
+  | .arrow _ _ => aesop
+
+end BaseFunctor
+
+section RecursionSchemes
+
+def Ty.fold
+    {α : Type}
+    (f : α → α → α)
+    (z : α)
+    (τ : Ty) :
+    α :=
+  match τ with
+  | .unit => z
+  | .arrow τ₁ τ₂ => f (Ty.fold f z τ₁) (Ty.fold f z τ₂)
+
+@[simp] theorem Ty.fold_unit : Ty.fold f z .unit = z := rfl
+@[simp] theorem Ty.fold_arrow {τ₁ τ₂ : Ty} {f : α → α → α} {z} :
+    Ty.fold f z (.arrow τ₁ τ₂) = f (Ty.fold f z τ₁) (Ty.fold f z τ₂) := rfl
+
+def Ty.accuM
+    [Monad m]
+    {α σ : Type}
+    (st : σ → σ × σ)
+    (f : α → α → σ → m α)
+    (z : σ → m α)
+    (t : Ty)
+    (i : σ) :
+    m α :=
+  match t with
+  | .unit => z i
+  | .arrow τ₁ τ₂ => do
+    let (s₁, s₂) := st i
+    f (← Ty.accuM st f z τ₁ s₁) (← Ty.accuM st f z τ₂ s₂) i
+
+@[simp] theorem Ty.accuM_unit
+  [Monad m] {α σ} {st : σ → σ × σ} {f : α → α → σ → m α} {z : σ → m α} {i : σ} :
+  Ty.accuM st f z (.unit : Ty) i = z i := rfl
+@[simp] theorem Ty.accuM_arrow
+  [Monad m] {α σ} {st : σ → σ × σ} {f : α → α → σ → m α} {z : σ → m α}
+      {i : σ} {τ₁ τ₂ : Ty} :
+  Ty.accuM st f z (.arrow τ₁ τ₂) i =
+   (do
+    let (s₁, s₂) := st i
+    f (← Ty.accuM st f z τ₁ s₁) (← Ty.accuM st f z τ₂ s₂) i) := by rfl
+
+end RecursionSchemes
+
+section Unfold
+
+open Gen
+
+private def Ty.unfold_aux (n : Nat) (f : α → Gen (TyF α)) (x : α) : Gen (Option Ty) :=
+  match n with
+  | 0 => pure none
+  | n + 1 => do
+    match (← f x) with
+    | .unit => pure (some .unit)
+    | .arrow b₁ b₂ => do
+      let τ₁ ← Ty.unfold_aux n f b₁
+      let τ₂ ← Ty.unfold_aux n f b₂
+      pure (do pure (.arrow (← τ₁) (← τ₂)))
+
+theorem Ty.unfold_aux_monotonic :
+    some v ∈ 〚Ty.unfold_aux n f x〛 →
+    some v ∈ 〚Ty.unfold_aux (n + m) f x〛 := by
+  induction n generalizing v f x
+  case zero =>
+    simp [Ty.unfold_aux]
+  case succ n' ih =>
+    unfold Ty.unfold_aux
+    simp
+    intro τ hτ h
+    cases τ <;> simp_all +arith
+    case unit =>
+      exists TyF.unit
+    case arrow τ₁ τ₂ =>
+      replace ⟨ ov₁, h₁, ov₂, h₂, h ⟩ := h
+      cases ov₁ <;> simp_all
+      case some v₁ =>
+        cases ov₂ <;> simp_all
+        case some v₂ =>
+        exists (TyF.arrow τ₁ τ₂)
+        simp_all
+        exists v₁
+        simp_all [ih]
+        exists v₂
+        simp_all [ih]
+
+@[irreducible]
+def Ty.unfold (f : α → Gen (TyF α)) (x : α) : Gen Ty :=
+  .indexed (λ n => Ty.unfold_aux n f x)
+
+@[simp]
+def Ty.unfold_support (P : α → TyF α → Prop) (x : α) (τ : Ty) : Prop :=
+  match τ with
+  | .unit => P x .unit
+  | .arrow τ₁ τ₂ => ∃ b₁ b₂,
+    P x (.arrow b₁ b₂) ∧
+    Ty.unfold_support P b₁ τ₁ ∧
+    Ty.unfold_support P b₂ τ₂
+
+@[simp]
+theorem Ty.support_unfold :
+    support (Ty.unfold f x) = Ty.unfold_support (λ x' => support (f x')) x := by
+  funext τ
+  simp_all
+  induction τ generalizing x with
+  | unit =>
+    apply Iff.intro
+    . intro h
+      simp_all [unfold]
+      replace ⟨n, h⟩ := h
+      cases n <;> simp_all [Ty.unfold_aux]
+      case succ n' =>
+        replace ⟨v', hv', h⟩ := h
+        cases v' <;> simp_all
+        case arrow τ₁ τ₂ =>
+          replace ⟨ov₁, h₁, ov₂, h₂, h⟩ := h
+          cases ov₁ <;> simp_all
+          cases ov₂ <;> simp_all
+    . intros h
+      simp_all [unfold]
+      exists 1
+      exists TyF.unit
+  | arrow τ₁ τ₂ ih₁ ih₂ =>
+    apply Iff.intro
+    . intro h
+      simp_all [unfold]
+      replace ⟨n, h⟩ := h
+      cases n <;> simp_all [Ty.unfold_aux]
+      case succ n =>
+        replace ⟨v', hv', h⟩ := h
+        cases v' <;> simp_all
+        case arrow b₁ b₂ =>
+          replace ⟨ov₁, hv₁, ov₂, hv₂, h⟩ := h
+          cases ov₁ <;> simp_all
+          case some v₁ =>
+            cases ov₂ <;> simp_all
+            case some v₂ =>
+              exists b₁, b₂
+              apply And.intro hv'
+              rw [← @ih₁ b₁, ← @ih₂ b₂]
+              apply And.intro <;> exists n
+    . intro ⟨b₁, b₂, hx, h₁, h₂⟩
+      rw [← @ih₁ b₁] at h₁
+      simp [unfold] at h₁ ⊢
+      replace ⟨n₁, h₁⟩ := h₁
+      rw [← @ih₂ b₂] at h₂
+      simp [unfold] at h₂
+      replace ⟨n₂, h₂⟩ := h₂
+      exists (n₁ + n₂ + 1)
+      simp_all
+      exists TyF.arrow b₁ b₂
+      simp_all
+      exists (some τ₁)
+      simp_all [Ty.unfold_aux_monotonic]
+      exists (some τ₂)
+      rw [Nat.add_comm]
+      simp_all [Ty.unfold_aux_monotonic]
+
+end Unfold
+
+section FoldConversions
+
+theorem Ty.fold_accu_Option_basic
+    {α : Type}
+    {v : α}
+    {τ : Ty}
+    {z : α}
+    {f : α → α → α} :
+    Ty.fold f z τ = v ↔
+    Ty.accuM
+      (fun _ => ((), ()))
+      (fun τ₁ τ₂ _ => some (f τ₁ τ₂))
+      (fun _ => some z)
+      τ
+      () = some v := by
+    induction τ generalizing v <;> simp_all [Ty.fold, Ty.accuM]
+    case arrow τ₁ τ₂ ih₁ ih₂ =>
+        replace ih₁ := @ih₁ (Ty.fold f z τ₁)
+        replace ih₂ := @ih₂ (Ty.fold f z τ₂)
+        simp_all [Ty.fold, Ty.accuM]
+
+theorem Ty.fold_accu_Option_true
+    {τ : Ty}
+    {f : Bool → Bool → Bool}
+    (h : ∀ acc₁ acc₂, f acc₁ acc₂ = (acc₁ && acc₂)) :
+    Ty.fold f true τ = true ↔
+    Ty.accuM
+      (fun _ => ((), ()))
+      (fun _ _ _ => some ())
+      (fun _ => some ())
+      τ
+      () = some () := by
+    induction τ <;> simp_all [Ty.fold, Ty.accuM]
+    case arrow τ₁ τ₂ ih₁ ih₂ =>
+        apply Iff.intro <;> intro hf
+        . -- (->)
+          generalize hv₁ : fold f true τ₁ = v₁
+          generalize hv₂ : fold f true τ₂ = v₂
+          cases v₁ <;> cases v₂ <;>
+            simp_all [Ty.fold, Ty.accuM, guard]
+        . -- (<-)
+          rw [Option.bind_eq_some] at hf
+          replace ⟨ v₁, hf ⟩ := hf
+          rw [Option.bind_eq_some] at hf
+          replace ⟨ h₁, ⟨ v₂, h₂ ⟩ ⟩ := hf
+          simp_all [Ty.fold, Ty.accuM, guard]
+
+theorem Ty.fold_accu_Option_function
+    {α σ : Type}
+    {i : σ}
+    {v : α}
+    {τ : Ty}
+    {z : (σ → α)}
+    {f : (σ → α) → (σ → α) → (σ → α)}
+    {g : α → α → σ → Option α}
+    {st₁ st₂ : σ → σ}
+    (h : ∀ acc₁ acc₂ s w,
+      f acc₁ acc₂ s = w ↔ (do g (← acc₁ (st₁ s)) (← acc₂ (st₂ s)) s) = some w)
+    :
+    Ty.fold f z τ i = v ↔
+    Ty.accuM
+      (fun s => (st₁ s, st₂ s))
+      g
+      (fun s => some (z s))
+      τ
+      i = some v := by
+    induction τ generalizing v i <;> simp_all [Ty.fold, Ty.accuM, Option.bind_eq_some]
+    case arrow τ₁ τ₂ ih₁ ih₂ =>
+      apply Iff.intro <;> intro hg
+      . -- (->)
+        exists (Ty.fold f z τ₁ (st₁ i))
+        rw [← ih₁] <;> simp_all
+        exists (Ty.fold f z τ₂ (st₂ i))
+        rw [← ih₂] <;> simp_all
+      . -- (<-)
+        replace ⟨ v₁, h₁, v₂, h₂, hg ⟩ := hg
+        rw [← ih₁] at h₁
+        rw [← ih₂] at h₂
+        rw [h₁, h₂]
+        apply hg
+
+theorem Ty.fold_accu_Option_function_true
+    {σ : Type}
+    {i : σ}
+    {τ : Ty}
+    {f : (σ → Bool) → (σ → Bool) → (σ → Bool)}
+    {g : σ → Bool}
+    {st₁ st₂ : σ → σ}
+    (h : ∀ acc₁ acc₂ s,
+      f acc₁ acc₂ s = true ↔ (do (return (g s) && (← acc₁ (st₁ s)) && (← acc₂ (st₂ s)))) = some true)
+    :
+    Ty.fold f (λ _ => true) τ i = true ↔
+    Ty.accuM
+      (fun s => (st₁ s, st₂ s))
+      (fun _ _ s => guard $ g s)
+      (fun _ => some ())
+      τ
+      i = some () := by
+    induction τ generalizing i <;> simp_all [Ty.fold, Ty.accuM, Option.bind_eq_some, guard]
+    case arrow τ₁ τ₂ ih₁ ih₂ =>
+      apply Iff.intro <;> intro hg <;> simp_all
+      replace ⟨⟨ v₁, h₁ ⟩, ⟨ v₂, h₂ ⟩ , hg⟩ := hg <;> simp_all
+
+end FoldConversions
+
+section FoldCoercion
+
+theorem Ty.coerce_to_fold
+    {τ : Ty}
+    {f : Ty → α} -- function to be coerced
+    {z : α}
+    {g : α → α → α}
+    (h₁ : f .unit = z)
+    (h₂ : ∀ τ₁ τ₂, f (.arrow τ₁ τ₂) = g (f τ₁) (f τ₂)) :
+    f τ = τ.fold g z := by
+  induction τ <;> simp_all
+
+end FoldCoercion
+
+section FoldMerging
+
+theorem Ty.merge_accuM
+    {τ : Ty}
+    {st₁ : σ₁ → σ₁ × σ₁}
+    {st₂ : σ₂ → σ₂ × σ₂}
+    {f₁ : α₁ → α₁ → σ₁ → Option α₁}
+    {f₂ : α₂ → α₂ → σ₂ → Option α₂}
+    {z₁ : σ₁ → Option α₁} {z₂ : σ₂ → Option α₂}
+    {i₁ : σ₁} {i₂ : σ₂}
+    {x₁ : α₁} {x₂ : α₂}
+    :
+    (τ.accuM st₁ f₁ z₁ i₁ = some x₁ ∧ τ.accuM st₂ f₂ z₂ i₂ = some x₂)
+    ↔
+    (τ.accuM
+      (λ (s₁, s₂) => (((st₁ s₁).1, (st₂ s₂).1), ((st₁ s₁).2, (st₂ s₂).2)))
+      (λ (x₁₁, x₁₂) (x₂₁, x₂₂) (s₁, s₂) => do (← f₁ x₁₁ x₂₁ s₁, ← f₂ x₁₂ x₂₂ s₂))
+      (λ (s₁, s₂) => do (← z₁ s₁, ← z₂ s₂))
+      (i₁, i₂) = some (x₁, x₂)) := by
+  induction τ generalizing i₁ i₂ x₁ x₂ <;> simp_all
+  case unit =>
+    apply Iff.intro <;> intro h
+    . -- (->)
+      rw [h.left, h.right]
+      simp
+    . -- (<-)
+      generalize hx₁ : (z₁ i₁) = x₁
+      generalize hx₂ : (z₂ i₂) = x₂
+      cases x₁ <;> cases x₂ <;> simp_all
+  case arrow τ₁ τ₂ ih₁ ih₂ =>
+    apply Iff.intro
+    . -- (->)
+      intro ⟨ h₁, h₂ ⟩
+      rw [Option.bind_eq_some] at h₁ h₂
+      replace ⟨ v₁₁, ⟨ hv₁₁, h₁ ⟩  ⟩ := @h₁
+      replace ⟨ v₁₂, ⟨ hv₁₂, h₂ ⟩  ⟩ := @h₂
+      rw [Option.bind_eq_some] at h₁ h₂
+      replace ⟨ v₂₁, ⟨ hv₂₁, h₁ ⟩  ⟩ := @h₁
+      replace ⟨ v₂₂, ⟨ hv₂₂, h₂ ⟩  ⟩ := @h₂
+      replace ih₁ := @ih₁ (st₁ i₁).1 (st₂ i₂).1 v₁₁ v₁₂
+      replace ih₂ := @ih₂ (st₁ i₁).2 (st₂ i₂).2 v₂₁ v₂₂
+      simp_all
+    . -- (<-)
+      intro h
+      rw [Option.bind_eq_some] at h
+      replace ⟨ ⟨ v₁₁, v₁₂ ⟩ , ⟨ h₁, h ⟩ ⟩ := @h
+      rw [Option.bind_eq_some] at h
+      replace ⟨ ⟨ v₂₁, v₂₂ ⟩ , ⟨ h₂, h ⟩ ⟩ := @h
+      rw [Option.bind_eq_some] at h
+      replace ⟨ v₁, ⟨ hv₁ , h ⟩ ⟩ := @h
+      rw [Option.bind_eq_some] at h
+      replace ⟨ v₂, ⟨ hv₂ , h ⟩ ⟩ := @h
+      replace ih₁ := @ih₁ (st₁ i₁).1 (st₂ i₂).1 v₁₁ v₁₂
+      replace ih₂ := @ih₂ (st₁ i₁).2 (st₂ i₂).2 v₂₁ v₂₂
+      simp_all
+
+end FoldMerging
+
+namespace Gen
+
+namespace CorrectGen
+
+@[reducible]
+def Ty.s_unfold
+    {α σ : Type}
+    {st : σ → σ × σ}
+    {f : α → α → σ → Option α}
+    {z : σ → Option α}
+    {s : σ}
+    {b : α}
+    (g : (b : α) → (s : σ) → CorrectGen
+      (fun (τ : TyF α) =>
+        (z s = some b ∧ τ = .unit) ∨
+        (∃ b₁ b₂, f b₁ b₂ s = some b ∧ τ = .arrow b₁ b₂))) :
+    CorrectGen (λ v => Ty.accuM st f z v s = some b) :=
+  Subtype.mk
+    (Ty.unfold (λ (b, s) => do
+      match (← (g b s).val) with
+      | .unit => pure .unit
+      | .arrow b₁ b₂ => pure (.arrow (b₁, (st s).1) (b₂, (st s).2))) (b, s)) <| by
+    rw [Ty.support_unfold]
+    funext τ
+    induction τ generalizing b s <;> simp_all
+    case unit =>
+      apply Iff.intro <;> intro h
+      . replace ⟨ τ', ⟨ hτ', h ⟩ ⟩ := h
+        cases τ' <;> simp_all [(g b s).property]
+      . exists TyF.unit
+        simp_all [(g b s).property]
+    case arrow τ₁ τ₂ ih₁ ih₂ =>
+      apply Iff.intro <;> intro h
+      . replace ⟨ b₁, s₁, b₂, s₂, ⟨ ⟨ τ', ⟨ hτ' , h ⟩  ⟩, ⟨ hτ₁, hτ₂ ⟩ ⟩ ⟩ := h
+        cases τ' <;> simp_all [(g b s).property]
+      . rw [Option.bind_eq_some] at h
+        replace ⟨ b₁, ⟨ h₁, h ⟩ ⟩ := h
+        rw [Option.bind_eq_some] at h
+        replace ⟨ b₂, ⟨ h₂, h ⟩ ⟩ := h
+        exists b₁, (st s).fst, b₂, (st s).snd
+        apply And.intro
+        . exists TyF.arrow b₁ b₂
+          simp_all [(g b s).property]
+        . simp_all [(g b s).property]
+
+end CorrectGen
+
+namespace Total
+
+@[simp]
+def Ty.total_unfold
+    (h : ∀ b, total (g b)) :
+    total (Ty.unfold g b) := by
+  simp [Ty.unfold]
+  apply total_indexed
+  intro n
+  induction n generalizing b with
+  | zero => simp [Ty.unfold_aux]
+  | succ n' ih =>
+    simp [Ty.unfold_aux]
+    apply total_bind <;> try apply h
+    intro τ h
+    cases τ <;> simp [ih]
+
+end Total
+
+namespace PrettyPrint
+
+def Ty.toString : Ty → String
+  | .unit => "()"
+  | .arrow τ₁ τ₂ => s!"({Ty.toString τ₁} → {Ty.toString τ₂})"
+
+instance : ToString Ty where
+  toString := Ty.toString
+
+end PrettyPrint
+
+end Gen
---
+++ Palamedes/Data/Stack.lean
@@ -0,0 +1,519 @@
+import Palamedes.Gen
+import Palamedes.CorrectGen
+import Palamedes.Total
+
+section TypeDef
+/- adapted from https://github.com/QuickChick/QuickChick/tree/master/examples/ifc-basic -/
+
+inductive Label where
+  | low
+  | high
+
+inductive Atom where
+  | atm (z : Int) (l : Label)
+
+inductive Stack where
+  | mty
+  | cons (a : Atom) (s : Stack)
+  | ret_cons (pc : Atom) (s : Stack)
+
+end TypeDef
+
+section BaseFunctor
+
+inductive StackF (α : Type) where
+  | mty : StackF α
+  | cons : (z : Atom) → (s : α) → StackF α
+  | ret_cons : (pc : Atom) → (s : α) → StackF α
+
+theorem StackF_or
+  {α : Type}
+  {P : Prop}
+  {Q : Atom → α → Prop}
+  {R : Atom → α → Prop}
+  {s : StackF α} :
+  StackF.rec P Q R s ↔
+    (P ∧ s = .mty)
+    ∨ (∃ z s', s = .cons z s' ∧ Q z s')
+    ∨ (∃ pc s', s = .ret_cons pc s' ∧ R pc s') := by
+  match s with
+  | .mty => simp
+  | .cons _ _ => aesop
+  | .ret_cons _ _ => aesop
+
+end BaseFunctor
+
+section RecursionSchemes
+
+def Stack.fold
+  {α : Type}
+  (f : (Atom ⊕ Atom) → α → α)
+  (z : α)
+  (s : Stack) : α :=
+  match s with
+  | .mty => z
+  | .cons x s' => f (Sum.inl x) (Stack.fold f z s')
+  | .ret_cons pc s' => f (Sum.inr pc) (Stack.fold f z s')
+
+@[simp] theorem Stack.fold_mty : Stack.fold f z .mty = z := rfl
+@[simp] theorem Stack.fold_cons {x} {s : Stack} {f : (Atom ⊕ Atom) → β → β} {z} :
+    Stack.fold f z (.cons x s) = f (Sum.inl x) (Stack.fold f z s) := rfl
+@[simp] theorem Stack.fold_ret_cons {x} {s : Stack} {f : (Atom ⊕ Atom) → β → β} {z} :
+    Stack.fold f z (.ret_cons x s) = f (Sum.inr x) (Stack.fold f z s) := rfl
+
+def Stack.accuM
+  [Monad m]
+  {α σ : Type}
+  (st : (Atom ⊕ Atom) → σ → σ)
+  (f : (Atom ⊕ Atom) → α → σ → m α)
+  (z : σ → m α)
+  (s : Stack)
+  (i : σ) : m α :=
+  match s with
+  | .mty => z i
+  | .cons x s' => do
+     f (Sum.inl x) (← Stack.accuM st f z s' (st (Sum.inl x) i)) i
+  | .ret_cons pc s' => do
+     f (Sum.inr pc) (← Stack.accuM st f z s' (st (Sum.inr pc) i)) i
+
+@[simp] theorem Stack.accuM_nil [Monad m] {st : (Atom ⊕ Atom) → σ → σ}
+  {f : (Atom ⊕ Atom) → β → σ → m β} {z : σ → m β} {i : σ} :
+  Stack.accuM st f z .mty i = z i := rfl
+@[simp] theorem Stack.accuM_cons  [Monad m] {st : (Atom ⊕ Atom) → σ → σ}
+  {f : (Atom ⊕ Atom) → β → σ → m β} {z : σ → m β} {i : σ} {x} {s : Stack} :
+    Stack.accuM st f z (.cons x s) i
+    = (do f (Sum.inl x) (← Stack.accuM st f z s (st (Sum.inl x) i)) i) := rfl
+@[simp] theorem Stack.accuM_ret_cons [Monad m] {st : (Atom ⊕ Atom) → σ → σ}
+  {f : (Atom ⊕ Atom) → β → σ → m β} {z : σ → m β} {i : σ} {pc} {s : Stack} :
+    Stack.accuM st f z (.ret_cons pc s) i
+    = (do f (Sum.inr pc) (← Stack.accuM st f z s (st (Sum.inr pc) i)) i) := rfl
+
+end RecursionSchemes
+
+section Unfold
+
+open Gen
+
+private def Stack.unfold_aux (n : Nat) (f : α → Gen (StackF α)) (x : α)
+  : Gen (Option Stack) :=
+  match n with
+  | 0 => pure none
+  | n' + 1 => do
+    match (← f x) with
+    | .mty => pure (some .mty)
+    | .cons x vs => do
+      let s ← Stack.unfold_aux n' f vs
+      pure (do pure (.cons x (← s)))
+    | .ret_cons pc vs => do
+      let s ← Stack.unfold_aux n' f vs
+      pure (do pure (.ret_cons pc (← s)))
+
+theorem Stack.unfold_aux_monotonic :
+    some v ∈ 〚Stack.unfold_aux n f b〛 →
+    some v ∈ 〚Stack.unfold_aux (n + 1) f b〛 := by
+  induction n generalizing v f b
+  case zero =>
+    simp [Stack.unfold_aux]
+  case succ n' ih =>
+    unfold Stack.unfold_aux
+    simp [bind]
+    intro h
+    replace ⟨ s, hs, h ⟩ := h
+    exists s
+    apply And.intro hs
+    cases s <;> simp_all [Functor.map, bind, Option.map]
+    all_goals
+      replace ⟨ v', h ⟩ := h <;>
+      exists v' <;>
+      cases v' <;> simp_all
+
+@[irreducible]
+def Stack.unfold (f : α → Gen (StackF α)) (x : α) : Gen Stack :=
+  .indexed (λ n => Stack.unfold_aux n f x)
+
+@[simp]
+def Stack.unfold_support (P : α → StackF α → Prop) (v : α) (s : Stack) : Prop :=
+  match s with
+  | .mty => P v .mty
+  | .cons x s' => ∃ v', P v (.cons x v') ∧ Stack.unfold_support P v' s'
+  | .ret_cons pc s' => ∃ v', P v (.ret_cons pc v') ∧ Stack.unfold_support P v' s'
+
+@[simp]
+theorem Stack.support_unfold :
+    support (Stack.unfold f b) = Stack.unfold_support (λ b' => support (f b')) b := by
+  funext s
+  simp_all
+  induction s generalizing b with
+  | mty =>
+    apply Iff.intro
+    . intro h
+      simp [unfold] at h
+      replace ⟨n, h⟩ := h
+      cases n <;> simp_all [Stack.unfold_aux]
+      case succ n' =>
+        have ⟨v', hv'1, hv'2⟩ := h
+        cases v' <;> simp_all
+        all_goals
+          have ⟨v'', hv''⟩ := hv'2
+          cases v'' <;> simp_all
+    . intros h
+      simp_all [unfold]
+      exists 1
+      exists StackF.mty
+  | cons x s' ih =>
+    apply Iff.intro
+    . intro h
+      simp [unfold] at h
+      replace ⟨n, h⟩ := h
+      cases n <;> simp_all [Stack.unfold_aux]
+      case succ n =>
+        have ⟨v', hv'1, hv'2⟩ := h; clear h
+        cases v' <;> simp_all
+        case cons _ b'' =>
+          have ⟨v'', hv''⟩ := hv'2; clear hv'2
+          cases v'' <;> simp_all
+          obtain ⟨hv'', rfl, rfl⟩ := hv''
+          exists b''
+          apply And.intro hv'1
+          apply (@ih b'').mp
+          simp [unfold]
+          exists n
+        case ret_cons _ b'' =>
+          have ⟨v'', hv''⟩ := hv'2
+          cases v'' <;> simp_all
+    . intro ⟨b', hx, hs⟩
+      simp_all [unfold]
+      have ⟨n, h⟩ := ih.mpr hs
+      exists n + 1
+      exists StackF.cons x b'
+      simp_all
+      exists (some s')
+  | ret_cons pc s' ih =>
+    apply Iff.intro
+    . intro h
+      simp [unfold] at h
+      replace ⟨n, h⟩ := h
+      cases n <;> simp_all [Stack.unfold_aux]
+      case succ n =>
+        replace ⟨v', hv', h⟩ := h
+        cases v' <;> simp_all
+        case cons _ b'' =>
+          have ⟨v'', hv''⟩ := h
+          cases v'' <;> simp_all
+        case ret_cons _ b'' =>
+          have ⟨v'', hv''⟩ := h
+          cases v'' <;> simp_all
+          obtain ⟨hv'', rfl, rfl⟩ := hv''
+          exists b''
+          apply And.intro hv'
+          apply (@ih b'').mp
+          simp [unfold]
+          exists n
+    . intro ⟨b', hx, hs⟩
+      simp_all [unfold]
+      have ⟨n, h⟩ := ih.mpr hs
+      exists (n + 1)
+      exists (StackF.ret_cons pc b')
+      simp_all
+      exists (some s')
+
+end Unfold
+
+section FoldConversions
+
+theorem Stack.fold_accu_Option_basic
+    {α : Type}
+    {v : α}
+    {s : Stack}
+    {z : α}
+    {f : (Atom ⊕ Atom) → α → α} :
+    Stack.fold f z s = v ↔
+    Stack.accuM
+      (fun _ _ => ())
+      (fun x s' _ => some (f x s'))
+      (fun _ => some z)
+      s
+      () = some v := by
+    induction s generalizing v <;> simp_all [Stack.fold, Stack.accuM]
+    case cons x s' ih =>
+      replace ih := @ih (Stack.fold f z s')
+      simp_all [Stack.fold, Stack.accuM]
+    case ret_cons pc s' ih =>
+      replace ih := @ih (Stack.fold f z s')
+      simp_all [Stack.fold, Stack.accuM]
+
+theorem Stack.fold_accu_Option_true
+    {g : (Atom ⊕ Atom) → Bool}
+    {f : (Atom ⊕ Atom) → Bool → Bool}
+    (h : ∀ x acc, f x acc = (g x && acc)) :
+    Stack.fold f true s = true ↔
+    Stack.accuM
+      (fun _ _ => ())
+      (fun x _ _ => guard (g x))
+      (fun _ => some ())
+      s
+      () = some () := by
+    induction s <;> simp_all [Stack.fold, Stack.accuM]
+    case cons x s' ih =>
+        apply Iff.intro <;> intro hf
+        . -- (->)
+          generalize hv : fold f true s' = v
+          cases v <;>
+            simp_all [Stack.fold, Stack.accuM, guard]
+        . -- (<-)
+          rw [Option.bind_eq_some] at hf
+          replace ⟨ v, hf ⟩ := hf
+          simp_all [Stack.fold, Stack.accuM, guard]
+    case ret_cons pc s' ih =>
+        apply Iff.intro <;> intro hf
+        . -- (->)
+          generalize hv : fold f true s' = v
+          cases v <;>
+            simp_all [Stack.fold, Stack.accuM, guard]
+        . -- (<-)
+          rw [Option.bind_eq_some] at hf
+          replace ⟨ v, hf ⟩ := hf
+          simp_all [Stack.fold, Stack.accuM, guard]
+
+theorem Stack.fold_accu_Option_function
+    {β σ : Type}
+    {i : σ}
+    {v : β}
+    {s : Stack}
+    {z : (σ → β)}
+    {f : (Atom ⊕ Atom) → (σ → β) → (σ → β)}
+    {g : (Atom ⊕ Atom) → β → σ → Option β}
+    {st : (Atom ⊕ Atom) → σ → σ}
+    (h : ∀ x acc s w,
+      f x acc s = w ↔ (do g x (← acc (st x s)) s) = some w)
+    :
+    Stack.fold f z s i = v ↔
+    Stack.accuM
+      st
+      g
+      (fun s => some (z s))
+      s
+      i = some v := by
+    induction s generalizing v i <;> simp_all [Stack.fold, Stack.accuM, Option.bind_eq_some]
+    case cons x s' ih =>
+      apply Iff.intro <;> intro hg
+      . -- (->)
+        exists (Stack.fold f z s' (st (Sum.inl x) i))
+        simp_all
+        rw [← ih]
+      . -- (<-)
+        replace ⟨w, ⟨hgw, hg⟩⟩ := hg
+        rw [← ih] at hgw
+        rw [hgw]
+        apply hg
+    case ret_cons pc s' ih =>
+      apply Iff.intro <;> intro hg
+      . -- (->)
+        exists (Stack.fold f z s' (st (Sum.inr pc) i))
+        simp_all
+        rw [← ih]
+      . -- (<-)
+        replace ⟨w, ⟨hgw, hg⟩⟩ := hg
+        rw [← ih] at hgw
+        rw [hgw]
+        apply hg
+
+theorem Stack.fold_accu_Option_function_true
+    {σ : Type}
+    {i : σ}
+    {s : Stack}
+    {f : (Atom ⊕ Atom) → (σ → Bool) → (σ → Bool)}
+    {g : (Atom ⊕ Atom) → σ → Bool}
+    {st :  (Atom ⊕ Atom) → σ → σ}
+    (h : ∀ x acc s',
+      f x acc s' = true ↔ (do (return (g x s') && (← acc (st x s')))) = some true)
+    :
+    Stack.fold f (λ _ => true) s i = true ↔
+    Stack.accuM
+      st
+      (fun x _ s => guard $ g x s)
+      (fun _ => some ())
+      s
+      i = some () := by
+    induction s generalizing i <;> simp_all [Stack.fold, Stack.accuM, Option.bind_eq_some, guard]
+    all_goals
+      (apply Iff.intro <;> intro hg <;> simp_all <;>
+      replace ⟨ ⟨ v, hv ⟩ , hg⟩ := hg <;> simp_all)
+
+
+end FoldConversions
+
+section FoldCoercion
+
+theorem Stack.coerce_to_fold
+    {s : Stack}
+    {f : Stack → α} -- function to be coerced
+    {z : α}
+    {g : (Atom ⊕ Atom) → α → α}
+    (hm : f .mty = z)
+    (hc : ∀ x s', f (.cons x s') = g (Sum.inl x) (f s'))
+    (hr : ∀ pc s', f (.ret_cons pc s') = g (Sum.inr pc) (f s')) :
+    f s = s.fold g z := by
+  induction s <;> simp_all
+
+end FoldCoercion
+
+section FoldMerging
+
+
+theorem Stack.merge_accuM
+    {s : Stack}
+    {st₁ : (Atom ⊕ Atom) → σ₁ → σ₁}
+    {st₂ : (Atom ⊕ Atom) → σ₂ → σ₂}
+    {f₁ : (Atom ⊕ Atom) → α₁ → σ₁ → Option α₁}
+    {f₂ : (Atom ⊕ Atom) → α₂ → σ₂ → Option α₂}
+    {s₁ : σ₁} {s₂ : σ₂}
+    {z₁ : σ₁ → Option α₁} {z₂ : σ₂ → Option α₂}
+    {v₁ : α₁} {v₂ : α₂}
+    :
+    (s.accuM st₁ f₁ z₁ s₁ = some v₁ ∧ s.accuM st₂ f₂ z₂ s₂ = some v₂)
+    ↔
+    (s.accuM
+      (λ x (s₁, s₂) => (st₁ x s₁, st₂ x s₂))
+      (λ x (v₁, v₂) (s₁, s₂) => do (← f₁ x v₁ s₁, ← f₂ x v₂ s₂))
+      (λ (s₁, s₂) => do (← z₁ s₁, ← z₂ s₂))
+      (s₁, s₂) = some (v₁, v₂)) := by
+    induction s generalizing st₁ st₂ f₁ f₂ s₁ s₂ z₁ z₂ v₁ v₂
+    case mty => simp_all [Stack.accuM, Option.bind_eq_some]
+    case cons y s' ih =>
+      simp_all [Stack.accuM, Option.bind_eq_some]
+      apply Iff.intro
+      . -- (->)
+        intro ⟨ ⟨ v₁', ⟨ hv1h, hv1tl ⟩ ⟩ , ⟨ v₂', ⟨ hv2h, hv2tl ⟩ ⟩ ⟩
+        exists v₁', v₂'
+        replace ih := @ih st₁ st₂ f₁ f₂ (st₁ (Sum.inl y) s₁) (st₂ (Sum.inl y) s₂) z₁ z₂ v₁' v₂'
+        simp_all [Stack.accuM, Option.bind_eq_some]
+      . -- (<-)
+        intro ⟨ v₁', v₂', h, h1, h2 ⟩
+        replace ih := @ih st₁ st₂ f₁ f₂ (st₁ (Sum.inl y) s₁) (st₂ (Sum.inl y) s₂) z₁ z₂ v₁' v₂'
+        apply And.intro
+        . exists v₁'
+          simp_all [Stack.accuM, Option.bind_eq_some]
+        . exists v₂'
+          simp_all [Stack.accuM, Option.bind_eq_some]
+    case ret_cons pc s' ih =>
+      simp_all [Stack.accuM, Option.bind_eq_some]
+      apply Iff.intro
+      . -- (->)
+        intro ⟨ ⟨ v₁', ⟨ hv1h, hv1tl ⟩ ⟩ , ⟨ v₂', ⟨ hv2h, hv2tl ⟩ ⟩ ⟩
+        exists v₁', v₂'
+        replace ih := @ih st₁ st₂ f₁ f₂ (st₁ (Sum.inr pc) s₁) (st₂ (Sum.inr pc) s₂) z₁ z₂ v₁' v₂'
+        simp_all [Stack.accuM, Option.bind_eq_some]
+      . -- (<-)
+        intro ⟨ v₁', v₂', h, h1, h2 ⟩
+        replace ih := @ih st₁ st₂ f₁ f₂ (st₁ (Sum.inr pc) s₁) (st₂ (Sum.inr pc) s₂) z₁ z₂ v₁' v₂'
+        apply And.intro
+        . exists v₁'
+          simp_all [Stack.accuM, Option.bind_eq_some]
+        . exists v₂'
+          simp_all [Stack.accuM, Option.bind_eq_some]
+
+end FoldMerging
+
+namespace Gen
+
+namespace CorrectGen
+
+@[reducible]
+def Stack.s_unfold
+    {α σ : Type}
+    {st : (Atom ⊕ Atom) → σ → σ}
+    {f : (Atom ⊕ Atom) → α → σ → Option α}
+    {z : σ → Option α}
+    {s : σ}
+    {b : α}
+    (g : (b : α) → (s : σ) → CorrectGen
+      (fun (x : StackF α) =>
+        (z s = some b ∧ x = .mty) ∨
+        (∃ a b', f (Sum.inl a) b' s = some b ∧ x = .cons a b') ∨
+        (∃ pc b', f (Sum.inr pc) b' s = some b ∧ x = .ret_cons pc b'))) :
+    CorrectGen (λ v => Stack.accuM st f z v s = some b) :=
+  Subtype.mk
+    (Stack.unfold (λ (b, s) => do
+      match (← (g b s).val) with
+      | .mty => pure .mty
+      | .cons a b' => pure (.cons a (b', (st (Sum.inl a) s)))
+      | .ret_cons pc b' => pure (.ret_cons pc (b', (st (Sum.inr pc) s)))) (b, s)) <| by
+    rw [Stack.support_unfold]
+    funext x
+    induction x generalizing b s <;> simp_all
+    case mty =>
+      apply Iff.intro <;> intro h
+      . replace ⟨ x, ⟨ hs, h ⟩ ⟩ := h
+        cases x <;> simp_all [(g b s).property]
+      . exists StackF.mty
+        simp_all [(g b s).property]
+    case cons a x' ih =>
+      apply Iff.intro <;> intro h
+      . replace ⟨ b', s', ⟨ ⟨ x'', ⟨ hx'', h ⟩ ⟩ , hx'⟩ ⟩ := h
+        cases x'' <;> simp_all [(g b s).property]
+      . rw [Option.bind_eq_some] at h
+        replace ⟨ b', hb', h ⟩ := h
+        exists b', st (Sum.inl a) s
+        apply And.intro
+        . exists StackF.cons a b'
+          simp_all [(g b s).property]
+        . simp_all [(g b s).property]
+    case ret_cons pc s' ih =>
+      apply Iff.intro <;> intro h
+      . replace ⟨ b', s', ⟨ ⟨ x'', ⟨ hx'', h ⟩ ⟩ , hx'⟩ ⟩ := h
+        cases x'' <;> simp_all [(g b s).property]
+      . rw [Option.bind_eq_some] at h
+        replace ⟨ b', hb', h ⟩ := h
+        exists b', st (Sum.inr pc) s
+        apply And.intro
+        . exists StackF.ret_cons pc b'
+          simp_all [(g b s).property]
+        . simp_all [(g b s).property]
+
+end CorrectGen
+
+namespace Total
+
+@[simp]
+def Stack.total_unfold
+    (h : ∀ b, total (g b)) :
+    total (Stack.unfold g b) := by
+  simp [Stack.unfold]
+  apply total_indexed
+  intro n
+  induction n generalizing b with
+  | zero => simp [Stack.unfold_aux]
+  | succ n' ih =>
+    simp [Stack.unfold_aux]
+    apply total_bind <;> try apply h
+    intro x h
+    cases x <;> simp [ih]
+
+end Total
+
+namespace PrettyPrint
+
+def Label.toString : Label → String
+  | .low => "low"
+  | .high => "high"
+
+instance : ToString Label where
+  toString := Label.toString
+
+def Atom.toString : Atom → String
+  | .atm z l => s!"({z} {l})"
+
+instance : ToString Atom where
+  toString := Atom.toString
+
+def Stack.toString : Stack → String
+  | .mty => "(empty)"
+  | .cons a s  => s!"(cons {a} {Stack.toString s})"
+  | .ret_cons pc s => s!"(ret_cons {pc} {Stack.toString s})"
+
+instance : ToString Stack where
+  toString := Stack.toString
+end PrettyPrint
+
+end Gen
