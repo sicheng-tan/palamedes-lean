7257ec93c8f596e4e090937d9f1b0e7d0706562f
cassiatorczon <cassiatorczon@gmail.com>
2025-06-23 22:02:08 -0400

    arbTy

--- Palamedes/Examples/STLC/TyTrue.lean
+++ Palamedes/Examples/STLC/TyTrue.lean
@@ -3,16 +3,20 @@ import Palamedes.Synthesizer
 
 open Gen CorrectGen
 
+def genTrueFold : Gen Ty := by
+  generator_search (fun τ => Ty.fold (fun b₁ b₂ => b₁ && b₂) true τ = true)
+
+@[simp]
+def recTrue : Ty → Bool
+  | .unit => true
+  | .arrow τ₁ τ₂ => recTrue τ₁ && recTrue τ₂
+
+def genTrueRec : Gen Ty := by
+  generator_search (fun τ => recTrue τ)
+
 set_option palamedes.debug true
 
-def arbTy : Gen Ty := by sorry
-  -- generator_search (fun v => Ty.fold (fun x x => true) true v = true)
-  -- let cg : CorrectGen (fun v => Ty.fold (fun x x => true) true v = true) := by
-  --   gapply (Ty.s_unfold)
-  -- let g : Gen (Ty) := by
-  --   optimize_gen cg.val
-  -- let _ : support cg.val = support g := by
-  --   optimality
-  -- let _ : Gen.total g := by
-  --   totality
-  -- exact g
+-- def genTrue : Gen Ty := by
+--   generator_search true
+
+def arbTy := genTrueRec
