e840fe1483d4376fddc1bec64b3ddd1871629f29
Harrison Goldstein <hgoldstein95@gmail.com>
2025-06-26 15:17:06 -0400

    Fix sorted between rec example

--- Palamedes/Examples/List/SortedBetween/SortedBetween.lean
+++ Palamedes/Examples/List/SortedBetween/SortedBetween.lean
@@ -3,22 +3,10 @@ import Palamedes.Synthesizer
 open Gen CorrectGen
 
 @[simp]
-def sortedBetweenRec (xs : List Nat) (lo hi : Nat) : Bool :=
+def sortedBetweenRec : List Nat → Nat × Nat → Bool := fun xs (lo, hi) =>
   match xs with
   | [] => true
-  | x :: xs' => lo <= x && x <= hi && sortedBetweenRec xs' x hi
-
-set_option palamedes.debug true
+  | x :: xs' => (lo <= x && x <= hi) && sortedBetweenRec xs' (x, hi)
 
 def genSortedBetween (lo hi : Nat) : Gen (List Nat) := by
-  -- generator_search (fun xs => sortedBetweenRec xs lo hi = true)
-  let cg : CorrectGen (fun xs => sortedBetweenRec xs lo hi = true) := by
-    conv => rhs; apply (List.coerce_to_fold (by rflm) (by intros; simp_all; rflm))
-    cgenerator_search
-  let g : Gen (List Nat) := by
-    optimize_gen cg.val
-  let _ : support cg.val = support g := by
-    optimality
-  let _ : Gen.total g := by
-    totality
-  exact g
+  generator_search (fun xs => sortedBetweenRec xs (lo, hi) = true)
