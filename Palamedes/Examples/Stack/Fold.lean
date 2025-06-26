80e9cf15dfaf838eb40f557f7af0f25dec227c6b
cassiatorczon <cassiatorczon@gmail.com>
2025-06-26 17:34:35 -0400

    commented out breaking examples

--- Palamedes/Examples/Stack/Fold.lean
+++ Palamedes/Examples/Stack/Fold.lean
@@ -2,30 +2,30 @@ import Palamedes.Synthesizer
 
 open Gen CorrectGen
 
-def isGoodInt : Int → Bool
-  | 0 => true
-  | 1 => true
-  | _ => false
+-- def isGoodInt : Int → Bool
+--   | 0 => true
+--   | 1 => true
+--   | _ => false
 
-def isGoodAtom : Atom → Bool
-  | .atm z _ => isGoodInt z
+-- def isGoodAtom : Atom → Bool
+--   | .atm z _ => isGoodInt z
 
-set_option palamedes.debug true
+-- set_option palamedes.debug true
 
-def genGoodStackFold (n : Nat) : Gen Stack := by
-  -- generator_search (λ s => Stack.fold (λ s => s == 0) (λ x acc s => isGoodAtom x && acc (s - 1)) (λ pc acc s => isGoodAtom pc && acc (s - 1)) s n = true)
-  let cg : CorrectGen (λ (s : Stack) => Stack.fold (λ s => s == 0) (λ x acc s => isGoodAtom x && acc (s - 1)) (λ pc acc s => isGoodAtom pc && acc (s - 1)) s n = true) := by
-    apply convert (by
-      funext
-      simp [guard, *]
-      rw [← Stack.fold_accu_Option_function_true]
-        <;> (try simp only [bind, Option.bind, pure, Option.some_inj, ← Bool.eq_iff_iff]; aesop); done
-      ) (Stack.s_unfold _)
-    sorry
-  let g : Gen (Stack) := by
-    optimize_gen cg.val
-  let _ : support cg.val = support g := by
-    optimality
-  let _ : Gen.total g := by
-    sorry -- totality
-  exact g
+-- def genGoodStackFold (n : Nat) : Gen Stack := by
+--   -- generator_search (λ s => Stack.fold (λ s => s == 0) (λ x acc s => isGoodAtom x && acc (s - 1)) (λ pc acc s => isGoodAtom pc && acc (s - 1)) s n = true)
+--   let cg : CorrectGen (λ (s : Stack) => Stack.fold (λ s => s == 0) (λ x acc s => isGoodAtom x && acc (s - 1)) (λ pc acc s => isGoodAtom pc && acc (s - 1)) s n = true) := by
+--     apply convert (by
+--       funext
+--       simp [guard, *]
+--       rw [← Stack.fold_accu_Option_function_true]
+--         <;> (try simp only [bind, Option.bind, pure, Option.some_inj, ← Bool.eq_iff_iff]; aesop); done
+--       ) (Stack.s_unfold _)
+--     sorry
+--   let g : Gen (Stack) := by
+--     optimize_gen cg.val
+--   let _ : support cg.val = support g := by
+--     optimality
+--   let _ : Gen.total g := by
+--     sorry -- totality
+--   exact g
--- Palamedes/Examples/Tree/RBT/Fold.lean
+++ Palamedes/Examples/Tree/RBT/Fold.lean
@@ -1,39 +1,3 @@
 import Palamedes.Synthesizer
 
 open Gen CorrectGen
-
-inductive Color where
-  | red
-  | black
-
-abbrev RBT α := Tree (Color × α)
-
-def rr_aux : RBT α → Bool → Bool := λ t isRedChild =>
- match t with
- | .leaf => true
- | .node l (.red, _) r => (not isRedChild) && rr_aux l true && rr_aux r true
- | .node l (.black, _) r => rr_aux l false && rr_aux r false
-
-@[simp]
-def rr : RBT α → Bool := λ t => rr_aux t false
-
-def bh : RBT α → Nat → Bool := λ t height =>
- match t with
- | .leaf => height == 1
- | .node l (.red, _) r => bh l height && bh r height
- | .node l (.black, _) r =>
-    height > 0 &&
-    bh l (height - 1) &&
-    bh r (height - 1)
-
-def RBT.isBST : Tree (α × Nat) → (Nat × Nat) → Bool := λ t ⟨lo, hi⟩ =>
-  match t with
-  | .leaf => true
-  | .node l (_, x) r =>
-    (lo <= x && x <= hi) &&
-    RBT.isBST l ⟨lo, x - 1⟩ &&
-    RBT.isBST r ⟨x + 1, hi⟩
-
-def genRBT (height lo hi : Nat) : Gen (RBT Nat) := by
-  -- generator_search (λ t => rr t ∧ bh t height ∧ RBT.isBST t (lo, hi))
-  sorry
