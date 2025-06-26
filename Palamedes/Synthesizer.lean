c6cb8c2704cf17f9fdabb4e9616d1516bbbf0051
Harrison Goldstein <hgoldstein95@gmail.com>
2025-06-26 17:28:23 -0400

    Add Option.bind_eq_some to simp_predicate

--- Palamedes/Synthesizer.lean
+++ Palamedes/Synthesizer.lean
@@ -78,7 +78,7 @@ macro "simp_predicate" : tactic =>
   `(tactic|
     first
       | funext
-        simp [guard, *]
+        simp [guard, Option.bind_eq_some, *]
         first
           | exact Eq.comm
           | simp_list_predicate
