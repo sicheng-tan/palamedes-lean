2a863524179a7e29a31785b905ef7810c832847e
cassiatorczon <cassiatorczon@gmail.com>
2025-06-26 21:17:50 -0400

    removed comment in netree fold

--- Palamedes/Examples/Tree/NETree/NETree.lean
+++ Palamedes/Examples/Tree/NETree/NETree.lean
@@ -2,24 +2,10 @@ import Palamedes.Synthesizer
 
 open Gen CorrectGen
 
--- @[simp]
--- def recNETree : Tree α → Bool
---   | .leaf => false
---   | .node l _ r => true && recNETree l && recNETree r
-
--- def genNETreeRec : Gen (Tree Nat) := by
---   generator_search (fun t => recNETree t = true)
-
 @[simp]
-def size : Tree α → Nat
-  | .leaf => 0
-  | .node l _ r => 1 + size l + size r
+def recNETree : Tree α → Bool
+  | .leaf => false
+  | .node l _ r => true && recNETree l && recNETree r
 
-@[simp]
-def isNETree (t : Tree α) : Bool :=
-  size t == 0
-
--- set_option palamedes.debug true
-
--- def genNETree : Gen (Tree Nat) := by
---   generator_search (fun t => isNETree t)
+def genNETree : Gen (Tree Nat) := by
+  generator_search (fun t => recNETree t = true)
