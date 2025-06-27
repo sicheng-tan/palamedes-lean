d947d20dd3d3f63364b71ae1c9aa2f8174639dc4
Harrison Goldstein <hgoldstein95@gmail.com>
2025-06-27 09:55:15 -0400

    Also simplify totality (same as optmality)

--- Palamedes/Synthesizer/Totality.lean
+++ Palamedes/Synthesizer/Totality.lean
@@ -1,10 +1,49 @@
-import Aesop
+import Palamedes.Data.List
+import Palamedes.Data.Stack
+import Palamedes.Data.STLC.Ty
+import Palamedes.Data.STLC.Term
+import Palamedes.Data.Tree
+import Palamedes.Data.Nat
+import Palamedes.Total
+
+open Gen Total
 
 macro "totality" : tactic =>
   `(tactic|
-    aesop
-      (rule_sets := [-default, -builtin, totality])
-      (config := {enableSimp := false})
-      (add safe (by intro))
-      (add 5% (by split))
-      (add 5% (by simp)))
+    -- NOTE: This is the old version. I think it mostly does the same thing, although it does have
+    -- the advantage of using Aesop's rule_set mechanism.
+
+    -- aesop
+    --   (rule_sets := [-default, -builtin, totality])
+    --   (config := {enableSimp := false})
+    --   (add safe (by intro))
+    --   (add 5% (by split))
+    --   (add 5% (by simp)))
+    repeat'
+      first
+        | (intro)
+        | apply total_pure
+        | apply total_bind
+        | apply total_pick
+        | apply total_assume
+        | apply total_indexed
+        | apply total_map
+        | apply total_dite
+        | apply total_arbBool
+        | apply total_Bool_rec
+        | apply total_arbNat
+        | apply total_Nat_rec
+        | apply total_choose
+        | apply total_gt
+        | apply Tree.total_unfold
+        | apply total_tuple
+        | apply Stack.total_unfold
+        | apply total_arbLabel
+        | apply total_arbAtom
+        | apply total_elements
+        | apply total_arbTy
+        | apply total_Ty_caseTy
+        | apply Term.total_unfold
+        | apply List.total_unfold
+        | split
+        | simp (config := {singlePass := true}))
