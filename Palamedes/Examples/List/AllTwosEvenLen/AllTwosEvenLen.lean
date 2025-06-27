5bc965fc6c48b8e5d07c02443cf53e418865871b
cassiatorczon <cassiatorczon@gmail.com>
2025-06-26 21:59:53 -0400

    list merge examples expanded version done

--- Palamedes/Examples/List/AllTwosEvenLen/AllTwosEvenLen.lean
+++ Palamedes/Examples/List/AllTwosEvenLen/AllTwosEvenLen.lean
@@ -1,3 +1,58 @@
 import Palamedes.Synthesizer
 
 open Gen CorrectGen
+
+namespace AllTwosEvenLen
+
+@[simp]
+def recAllTwos : List Nat → Bool
+  | [] => true
+  | x :: xs => x = 2 && recAllTwos xs
+
+@[simp]
+def recEvenLen : List α → Bool
+  | [] => true
+  | _ :: xs => !(recEvenLen xs)
+
+set_option palamedes.debug true
+
+def genAllTwosEvenLen : Gen (List Nat) := by
+  -- generator_search (fun xs => (recAllTwos xs && recEvenLen xs) = true)
+  let cg : CorrectGen (fun (xs: List Nat) => (recAllTwos xs && recEvenLen xs) = true) := by
+    -- NOTE: this is exactly the same as the fold version
+    apply convert (by
+      funext
+      simp [guard, *]
+      rw [← List.merge_accuM]
+      apply and_congr
+      . simp_list_predicate
+      . simp_list_predicate
+    ) (List.s_unfold _)
+    intros s b
+    replace ⟨s1, s2⟩ := s
+    apply caseBool s2
+    . intro
+      gapply (s_pick _ _)
+      . cgenerator_search
+      . apply convert (by
+          funext
+          simp [guard, *, Option.bind_eq_some]
+          apply exists_congr; intro; rw [true_and]) (s_bind _ _)
+        . cgenerator_search
+        . cgenerator_search
+    . intro
+      apply convert (by
+        funext
+        simp [guard, *]
+        rw [exists_comm]
+        apply exists_congr; intro; rw [true_and]
+      ) (s_bind _ _)
+      . cgenerator_search
+      . intro
+        apply convert (by
+          funext
+          simp [guard, *, Option.bind_eq_some]
+          rfl) (s_pure _)
+  let g : Gen (List Nat) := by
+    optimize_gen cg.val
+  exact g
--- Palamedes/Examples/List/LengthKAllTwos/LengthKAllTwos.lean
+++ Palamedes/Examples/List/LengthKAllTwos/LengthKAllTwos.lean
@@ -1,3 +1,52 @@
 import Palamedes.Synthesizer
 
 open Gen CorrectGen
+
+namespace LengthKAllTwos
+
+@[simp]
+def recAllTwos : List Nat → Bool
+  | [] => true
+  | x :: xs => x = 2 && recAllTwos xs
+
+set_option palamedes.debug true
+
+def genLengthKAllTwos (k : Nat): Gen (List Nat) := by
+  -- generator_search (fun xs => (decide (xs.length = k) && recAllTwos xs) = true)
+  let cg : CorrectGen (fun xs => (decide (xs.length = k) && recAllTwos xs) = true) := by
+    -- NOTE: this is exactly the same as the fold version
+    apply convert (by
+      funext
+      simp [guard, *]
+      rw [← List.merge_accuM]
+      apply and_congr
+      . simp_list_predicate
+      . simp_list_predicate
+    ) (List.s_unfold _)
+    intros b s
+    replace ⟨ n , () ⟩ := b
+    apply caseNat (by assumption)
+    . intros
+      apply convert (by
+        funext
+        simp [guard, *, Option.bind_eq_some]
+        rfl) (s_pure _)
+    . intros
+      apply convert (by
+        funext
+        simp [guard, *, Option.bind_eq_some, and_assoc]
+        apply exists_congr
+        intro
+        rw [true_and]
+        ) (s_bind _ _)
+      . cgenerator_search
+      . cgenerator_search
+  let g : Gen (List Nat) := by
+    optimize_gen cg.val
+  let _ : support cg.val = support g := by
+    optimality
+  let _ : Gen.total g := by
+    totality
+  exact g
+
+end LengthKAllTwos
