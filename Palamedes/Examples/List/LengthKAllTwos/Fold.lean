879f7051d73f9894bc53a76b383b05ce00e4546e
cassiatorczon <cassiatorczon@gmail.com>
2025-06-26 21:46:05 -0400

    lengthKAllTwosFold done

--- Palamedes/Examples/List/LengthKAllTwos/Fold.lean
+++ Palamedes/Examples/List/LengthKAllTwos/Fold.lean
@@ -2,8 +2,57 @@ import Palamedes.Synthesizer
 
 open Gen CorrectGen
 
--- def genLengthKAllTwosFold (k : Nat) : Gen (List Nat) := by
---   -- generator_search (fun (xs : List Nat) => List.fold (fun x l => l + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true)
+set_option palamedes.debug true
+
+def genLengthKAllTwosFold (k : Nat) : Gen (List Nat) := by
+  -- generator_search (fun xs => List.fold (fun x b => b + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true)
+  let cg : CorrectGen (fun xs => List.fold (fun x b => b + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true) := by
+    apply convert (by
+      funext
+      simp [guard, *]
+      rw [← List.merge_accuM]
+      apply and_congr
+      . simp_list_predicate
+      . simp_list_predicate
+    ) (List.s_unfold _)
+    intros b s
+    replace ⟨ n , () ⟩ := b
+    apply caseNat (by assumption)
+    . intros
+      apply convert (by
+        funext
+        simp [guard, *, Option.bind_eq_some]
+        rfl) (s_pure _)
+    . intros
+      apply convert (by
+        funext
+        simp [guard, *, Option.bind_eq_some, and_assoc]
+        apply exists_congr
+        intro
+        rw [true_and]
+        ) (s_bind _ _)
+      . cgenerator_search
+      . cgenerator_search
+  let g : Gen (List Nat) := by
+    optimize_gen cg.val
+  let _ : support cg.val = support g := by
+    optimality
+  let _ : Gen.total g := by
+    totality
+  exact g
+
+
+
+
+
+
+
+
+
+
+
+
+
 --   let cg : CorrectGen (fun (xs : List Nat) => List.fold (fun x l => l + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true) := by
 --     apply convert (by
 --       funext
