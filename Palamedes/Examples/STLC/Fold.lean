da630a45e7289e9a006b332a013b2dacf056a99b
Harrison Goldstein <hgoldstein95@gmail.com>
2025-06-28 12:22:16 -0400

    No longer need manual unfolds in WellTyped

--- Palamedes/Examples/STLC/Fold.lean
+++ Palamedes/Examples/STLC/Fold.lean
@@ -27,13 +27,20 @@ def genWellTypedFold (Γ : List Ty) : Gen Term := by
             . cgenerator_search
           . apply convert (by
             funext
-            unfold getTypeFold.match_1
-            simp_all [Ty.deforest_eq, Ty.as_or, Option.bind_eq_some_iff]
+            simp
             rw [exists_comm]
             apply exists_congr; intro; rw [true_and]
             ) (s_bind _ _)
             . cgenerator_search
-            . cgenerator_search
+            . intro
+              apply (s_bind _ _)
+              . apply convert
+                  (by
+                    funext
+                    split <;> aesop (add simp Option.bind_eq_some_iff))
+                  (s_pure _)
+              . intro
+                apply (s_pure _)
       . intros
         gapply (s_pick _ _)
         . apply (s_bind _ _)
@@ -41,15 +48,22 @@ def genWellTypedFold (Γ : List Ty) : Gen Term := by
           . cgenerator_search
         . gapply (s_pick _ _)
           . apply convert (by funext; rfl) (s_pure _)
-          . apply convert (by
-            funext
-            unfold getTypeFold.match_1
-            simp_all [Ty.deforest_eq, Ty.as_or, Option.bind_eq_some_iff]
-            rw [exists_comm]
-            apply exists_congr; intro; rw [true_and]
-            ) (s_bind _ _)
+          . apply convert
+              (by
+                funext
+                rw [exists_comm]
+                congr; funext; rw [true_and])
+              (s_bind _ _)
             . cgenerator_search
-            . cgenerator_search
+            . intro
+              apply (s_bind _ _)
+              . apply convert
+                  (by
+                    funext
+                    split <;> aesop (add simp Option.bind_eq_some_iff))
+                  (s_pure _)
+              . intro
+                apply (s_pure _)
   let g : Gen (Term) := by
     optimize_gen cg.val
   have support_eq : support cg.val = support g := by
