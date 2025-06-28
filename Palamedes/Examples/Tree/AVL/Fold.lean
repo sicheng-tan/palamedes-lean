1dd3fe2589a24d2fb825307fb3dc994879da74aa
Harrison Goldstein <hgoldstein95@gmail.com>
2025-06-28 10:31:04 -0400

    Getting AVLFold closer to making sense

--- Palamedes/Examples/Tree/AVL/Fold.lean
+++ Palamedes/Examples/Tree/AVL/Fold.lean
@@ -7,6 +7,40 @@ set_option palamedes.debug true
 
 namespace AVLFold
 
+def duncurry
+    {F : α × β → Type u} :
+    ((a : α) → (b : β) → F (a, b)) → (p : α × β) → F p :=
+  fun f p => f p.1 p.2
+
+def splitNat
+    (n : Nat)
+    (z : (n = 0) → α)
+    (s : (n' : Nat) → (n = n' + 1) → α) :
+    α :=
+  match n with
+  | 0 => z rfl
+  | n' + 1 => s n' rfl
+
+def support_splitNat_congr
+    {z z' : n = 0 → Gen α}
+    {s s' : (n' : Nat) → n = n' + 1 → Gen α}
+    (hz : ∀ {h}, support (z h) = support (z' h))
+    (hz : ∀ {n} {h}, support (s n h) = support (s' n h)) :
+    support (splitNat n z s) = support (splitNat n z' s') := by
+  aesop (add simp splitNat)
+
+@[reducible]
+def s_splitNat
+    (n : Nat)
+    (gz : (n = 0) → @CorrectGen α P)
+    (gs : (n' : Nat) → (n = n' + 1) → @CorrectGen α P) :
+    @CorrectGen α P :=
+    Subtype.mk
+      (splitNat n (fun h => (gz h).val) (fun a b => (gs a b).val)) <| by
+    match n with
+    | 0 => exact (gz _).property
+    | n' + 1 => exact (gs _ _).property
+
 def genAVL (height lo hi : Nat) : Gen (Tree Nat) := by
    -- generator_search (fun t =>
   -- Tree.fold (fun bl x br h => decide (h > 0) && bl (h - 1) && br (h - 1)) (fun h => decide (h ≤ 1)) t height = true ∧
@@ -16,7 +50,7 @@ def genAVL (height lo hi : Nat) : Gen (Tree Nat) := by
   --         | (sl, sr) => decide (sl ≤ x) && decide (x ≤ sr) && bl (sl, x - 1) && br (x + 1, sr))
   --       (fun x => true) t (lo, hi) =
   --     true)
-  let cg : CorrectGen (fun (t : Tree Nat) =>
+  letI cg : CorrectGen (fun (t : Tree Nat) =>
       Tree.fold
           (fun bl x br bounds =>
             match bounds with
@@ -33,12 +67,11 @@ def genAVL (height lo hi : Nat) : Gen (Tree Nat) := by
         . simp_tree_predicate
         ) (Tree.s_unfold _)
       intros b s
-      replace ⟨ ⟨ low, high ⟩ , h ⟩ := s
-      apply caseNat (by assumption)
-      . intro h'
-        cgenerator_search
-      . intros n' h'
-        apply caseNat (by assumption)
+      apply s_splitNat s.2
+      . cgenerator_search
+      . intro n'
+        intro
+        apply s_splitNat n'
         . intros
           gapply (s_pick _ _)
           . cgenerator_search
@@ -52,8 +85,13 @@ def genAVL (height lo hi : Nat) : Gen (Tree Nat) := by
             ) (s_bind _ _)
           . cgenerator_search
           . cgenerator_search
-  let g : Gen (Tree Nat) := by
+  letI g : Gen (Tree Nat) := by
     optimize_gen cg.val
   let _ : support cg.val = support g := by
     optimality
+    apply support_splitNat_congr
+    . optimality
+    . optimality
+      apply support_splitNat_congr
+      optimality
   exact g
