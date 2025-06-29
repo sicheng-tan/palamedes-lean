44c623b76cb70e1b7f90c70e2058468023e5b59a
cassiatorczon <cassiatorczon@gmail.com>
2025-06-29 18:45:36 -0400

    avl done

--- Palamedes/Examples/Tree/AVL/AVL.lean
+++ Palamedes/Examples/Tree/AVL/AVL.lean
@@ -1,8 +1,19 @@
 import Palamedes.Synthesizer
-import Palamedes.Examples.Tree.BST.BST
 
 open Gen CorrectGen
 
+namespace AVL
+
+@[simp]
+def isBST : Tree Nat → (Nat × Nat) → Bool := λ t ⟨lo, hi⟩ =>
+  match t with
+  | .leaf => true
+  | .node l x r =>
+    (lo <= x && x <= hi) &&
+    isBST l ⟨lo, x - 1⟩ &&
+    isBST r ⟨x + 1, hi⟩
+
+@[simp]
 def isBalanced : Tree Nat → Nat → Bool := fun t height =>
   match t with
   | .leaf => height <= 1
@@ -11,24 +22,9 @@ def isBalanced : Tree Nat → Nat → Bool := fun t height =>
     isBalanced l (height - 1) &&
     isBalanced r (height - 1)
 
-set_option palamedes.debug true
+set_option maxHeartbeats 1000000
 
 def genAVL (height lo hi : Nat) : Gen (Tree Nat) := by
-  -- generator_search (fun t => isBalanced t height = true ∧ isBST t (lo, hi) = true)
-  let cg : CorrectGen (fun t => isBalanced t height = true ∧ isBST t (lo, hi) = true) := by
-    -- apply convert (by
-    --   funext
-    --   simp [guard, *]
-    --   rw [← Tree.merge_accuM]
-    --   apply and_congr
-    --   . simp_tree_predicate
-    --   . simp_tree_predicate
-    -- ) (Tree.s_unfold _)
-    sorry
-  let g : Gen (Tree Nat) := by
-    optimize_gen cg.val
-  -- let _ : support cg.val = support g := by
-  --   optimality
-  -- let _ : Gen.total g := by
-  --   totality
-  exact g
+  generator_search (fun t => isBST t (lo, hi) = true ∧ isBalanced t height = true) allow_partial
+
+end AVL
