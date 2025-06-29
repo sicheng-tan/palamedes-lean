442fea1efe2c0e5dc30916d31177906a04af33e2
cassiatorczon <cassiatorczon@gmail.com>
2025-06-28 23:33:41 -0400

    alltwosevenlen done

--- Palamedes/Examples/List/AllTwosEvenLen/AllTwosEvenLen.lean
+++ Palamedes/Examples/List/AllTwosEvenLen/AllTwosEvenLen.lean
@@ -14,39 +14,5 @@ def recEvenLen : List α → Bool
   | [] => true
   | _ :: xs => !(recEvenLen xs)
 
-set_option palamedes.debug true
-
 def genAllTwosEvenLen : Gen (List Nat) := by
-  -- generator_search (fun xs => (recAllTwos xs && recEvenLen xs) = true)
-  let cg : CorrectGen (fun (xs: List Nat) => (recAllTwos xs && recEvenLen xs) = true) := by
-    -- NOTE: this is exactly the same as the fold version
-    apply convert (by norm_for_List_unfold) (List.s_unfold _)
-    sorry
-    -- intros s b
-    -- replace ⟨s1, s2⟩ := s
-    -- apply caseBool s2
-    -- . intro
-    --   gapply (s_pick _ _)
-    --   . cgenerator_search
-    --   . apply convert (by
-    --       funext
-    --       simp [guard, *, Option.bind_eq_some_iff]
-    --       apply exists_congr; intro; rw [true_and]) (s_bind _ _)
-    --     . cgenerator_search
-    --     . cgenerator_search
-    -- . intro
-    --   apply convert (by
-    --     funext
-    --     simp [guard, *]
-    --     rw [exists_comm]
-    --     apply exists_congr; intro; rw [true_and]
-    --   ) (s_bind _ _)
-    --   . cgenerator_search
-    --   . intro
-    --     apply convert (by
-    --       funext
-    --       simp [guard, *, Option.bind_eq_some_iff]
-    --       rfl) (s_pure _)
-  let g : Gen (List Nat) := by
-    optimize_gen cg.val
-  exact g
+  generator_search (fun xs => (recAllTwos xs && recEvenLen xs) = true)
--- Palamedes/Examples/List/AllTwosEvenLen/Fold.lean
+++ Palamedes/Examples/List/AllTwosEvenLen/Fold.lean
@@ -3,47 +3,4 @@ import Palamedes.Synthesizer
 open Gen CorrectGen
 
 def genAllTwosEvenLenFold : Gen (List Nat) := by
-  -- generator_search (fun xs => List.fold (fun x b => x == 2 && b) true xs = true ∧ List.fold (fun x b => !b) true xs = true)
-  let cg : CorrectGen (fun (xs : List Nat) => List.fold (fun x b => x == 2 && b) true xs = true ∧ List.fold (fun x b => !b) true xs = true) := by
-    apply convert (by
-      funext
-      simp [guard, *]
-      rw [← List.merge_accuM]
-      -- apply and_congr
-      -- . simp_list_predicate
-      -- . simp_list_predicate
-      all_goals sorry
-    ) (List.s_unfold _)
-    sorry
-    -- intros s b
-    -- replace ⟨s1, s2⟩ := s
-    -- apply caseBool s2
-    -- . intro
-    --   gapply (s_pick _ _)
-    --   . cgenerator_search
-    --   . apply convert (by
-    --       funext
-    --       simp [guard, *, Option.bind_eq_some_iff]
-    --       apply exists_congr; intro; rw [true_and]) (s_bind _ _)
-    --     . cgenerator_search
-    --     . cgenerator_search
-    -- . intro
-    --   apply convert (by
-    --     funext
-    --     simp [guard, *]
-    --     rw [exists_comm]
-    --     apply exists_congr; intro; rw [true_and]
-    --   ) (s_bind _ _)
-    --   . cgenerator_search
-    --   . intro
-    --     apply convert (by
-    --       funext
-    --       simp [guard, *, Option.bind_eq_some_iff]
-    --       rfl) (s_pure _)
-  let g : Gen (List Nat) := by
-    optimize_gen cg.val
-  let _ : support cg.val = support g := by
-    optimality
-  -- let _ : Gen.total g := by
-  --   totality
-  exact g
+  generator_search (fun xs => List.fold (fun x b => x == 2 && b) true xs = true ∧ List.fold (fun x b => !b) true xs = true)
