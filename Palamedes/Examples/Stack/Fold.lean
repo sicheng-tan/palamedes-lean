c39f95e0cd6f3bf177e9a9b89ea183234b3f36b2
cassiatorczon <cassiatorczon@gmail.com>
2025-06-28 22:39:14 -0400

    switched stack fold to use rename_i version of casenat

--- Palamedes/Examples/Stack/Fold.lean
+++ Palamedes/Examples/Stack/Fold.lean
@@ -17,17 +17,10 @@ def isGoodAtom : Atom → Bool
 def genGoodStackFold (n : Nat) : Gen Stack := by
   -- generator_search (λ s => Stack.fold (λ s => s == 0) (λ x acc s => isGoodAtom x && acc (s - 1)) (λ pc acc s => isGoodAtom pc && acc (s - 1)) s n = true)
   let cg : CorrectGen (λ (s : Stack) => Stack.fold (λ i => i == 0) (λ x acc i => i > 0 && isGoodAtom x && acc (i - 1)) (λ pc acc i => i > 0 && isGoodAtom pc && acc (i - 1)) s n = true) := by
-    apply convert (by
-      norm_for_Stack_unfold
-      -- funext
-      -- simp_predicate
-      -- rw [← Stack.fold_accu_Option_function_true]
-      --   <;> (try simp only [bind, Option.bind, pure, Option.some_inj, ← Bool.eq_iff_iff]; aesop); done
-      ) (Stack.s_unfold _)
-    intros b i
-    -- rename_i n
-    -- apply s_caseNat n
-    apply s_caseNat (by assumption)
+    apply convert (by norm_for_Stack_unfold ) (Stack.s_unfold _)
+    intros
+    rename_i n
+    apply s_caseNat n
     . cgenerator_search
     . cgenerator_search
   let g : Gen (Stack) := by
