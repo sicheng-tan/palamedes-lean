668d8a61bf8fcf624c0a17d78fe4c35dc956c303
cassiatorczon <cassiatorczon@gmail.com>
2025-06-28 23:03:05 -0400

    stack automated except unfold

--- Palamedes/Examples/Stack/Fold.lean
+++ Palamedes/Examples/Stack/Fold.lean
@@ -18,10 +18,7 @@ def genGoodStackFold (n : Nat) : Gen Stack := by
   -- generator_search (λ s => Stack.fold (λ s => s == 0) (λ x acc s => isGoodAtom x && acc (s - 1)) (λ pc acc s => isGoodAtom pc && acc (s - 1)) s n = true)
   let cg : CorrectGen (λ (s : Stack) => Stack.fold (λ i => i == 0) (λ x acc i => i > 0 && isGoodAtom x && acc (i - 1)) (λ pc acc i => i > 0 && isGoodAtom pc && acc (i - 1)) s n = true) := by
     apply convert (by norm_for_Stack_unfold ) (Stack.s_unfold _)
-    intros
-    rename_i n; apply s_caseNat n
-    . cgenerator_search
-    . cgenerator_search
+    cgenerator_search
   let g : Gen (Stack) := by
     optimize_gen cg.val
   let _ : support cg.val = support g := by
