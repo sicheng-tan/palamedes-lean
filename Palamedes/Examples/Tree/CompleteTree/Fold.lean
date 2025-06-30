355be35c84ee26b437bc9029bcfa1f5068905aef
cassiatorczon <cassiatorczon@gmail.com>
2025-06-30 01:14:09 -0400

    unsorryed completetreefold

--- Palamedes/Examples/Tree/CompleteTree/Fold.lean
+++ Palamedes/Examples/Tree/CompleteTree/Fold.lean
@@ -3,5 +3,4 @@ import Palamedes.Synthesizer
 open Gen CorrectGen
 
 def genCompleteTreeFold (n : Nat) : Gen (Tree Nat) := by
- -- generator_search (fun t => Tree.fold (fun bl x br s => decide (s > 0) && bl (s - 1) && br (s - 1)) (fun s => s == 0) t n = true)
-  sorry
+  generator_search (fun t => Tree.fold (fun bl x br s => decide (s > 0) && bl (s - 1) && br (s - 1)) (fun s => s == 0) t n = true)
