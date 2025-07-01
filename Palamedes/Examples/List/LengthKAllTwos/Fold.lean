fb1e9f20f4289cf5924465c58e0b9de9c0d2ca86
cassiatorczon <cassiatorczon@gmail.com>
2025-07-01 18:43:08 -0400

    lengthkalltwos done

--- Palamedes/Examples/List/LengthKAllTwos/Fold.lean
+++ Palamedes/Examples/List/LengthKAllTwos/Fold.lean
@@ -1,36 +1,7 @@
 import Palamedes.Synthesizer
+import Palamedes.Synthesizer.Util
 
 open Gen CorrectGen
 
-set_option palamedes.debug true
-
 def genLengthKAllTwosFold (k : Nat) : Gen (List Nat) := by
-  -- generator_search (fun xs => List.fold (fun x b => b + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true)
-  let cg : CorrectGen (fun xs => List.fold (fun x b => b + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true) := by
-    apply convert (by norm_for_List_unfold) (List.s_unfold _)
-    (repeat apply duncurry); intro
-    (repeat apply duncurry); intro
-    (repeat apply duncurry); intro
-    (repeat apply duncurry); intro
-    rename_i n _ _ _
-    apply s_caseNat n (by intros; rflm)
-    . cgenerator_search
-    . (repeat apply duncurry); intro
-      apply convert (by
-        funext
-        /- PROBLEM: the and_assoc is the issue
-           (adding it in simp_predicate breaks other things) -/
-        try simp [guard, Option.bind_eq_some_iff, and_assoc, *]
-        first
-          | rfl
-          | apply exists_congr; intro; rw [true_and]
-        ) (s_bind _ _)
-      . cgenerator_search
-      . cgenerator_search
-  let g : Gen (List Nat) := by
-    optimize_gen cg.val
-  let _ : support cg.val = support g := by
-    optimality
-  let _ : Gen.total g := by
-    totality
-  exact g
+  generator_search (fun xs => List.fold (fun x b => b + 1) 0 xs = k ∧ List.fold (fun x b => x == 2 && b) true xs = true)
--- Palamedes/Examples/List/LengthKAllTwos/LengthKAllTwos.lean
+++ Palamedes/Examples/List/LengthKAllTwos/LengthKAllTwos.lean
@@ -9,46 +9,7 @@ def recAllTwos : List Nat → Bool
   | [] => true
   | x :: xs => x = 2 && recAllTwos xs
 
-set_option palamedes.debug true
-
 def genLengthKAllTwos (k : Nat): Gen (List Nat) := by
-  -- generator_search (fun xs => (decide (xs.length = k) && recAllTwos xs) = true)
-  let cg : CorrectGen (fun xs => (decide (xs.length = k) && recAllTwos xs) = true) := by
-    -- NOTE: this is exactly the same as the fold version
-    apply convert (by
-      funext
-      simp [guard, *]
-      rw [← List.merge_accuM]
-      all_goals sorry
-      -- apply and_congr
-      -- . simp_list_predicate
-      -- . simp_list_predicate
-    ) (List.s_unfold _)
-    sorry
-    -- intros b s
-    -- replace ⟨ n , () ⟩ := b
-    -- apply s_caseNat (by assumption)
-    -- . intros
-    --   apply convert (by
-    --     funext
-    --     simp [guard, *, Option.bind_eq_some_iff]
-    --     rfl) (s_pure _)
-    -- . intros
-    --   apply convert (by
-    --     funext
-    --     simp [guard, *, Option.bind_eq_some_iff, and_assoc]
-    --     apply exists_congr
-    --     intro
-    --     rw [true_and]
-    --     ) (s_bind _ _)
-    --   . cgenerator_search
-    --   . cgenerator_search
-  let g : Gen (List Nat) := by
-    optimize_gen cg.val
-  let _ : support cg.val = support g := by
-    optimality
-  -- let _ : Gen.total g := by
-  --   totality
-  exact g
+  generator_search (fun xs => (decide (xs.length = k) && recAllTwos xs) = true)
 
 end LengthKAllTwos
