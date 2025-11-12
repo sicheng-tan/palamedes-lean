4a823de77428a163fbeb3c979b20b469cf0cd8ea
cassiatorczon <cassiatorczon@gmail.com>
2025-11-11 21:40:54 -0500

    a few accuOpts

--- Palamedes/Examples/List/AllEvens/AccuOpt.lean
+++ Palamedes/Examples/List/AllEvens/AccuOpt.lean
@@ -11,23 +11,4 @@ def isAllEvensAccuOpt (xs : List Nat) : Option Unit :=
 def genAllEvensAccuOpt : Gen (List Nat) := by
   generator_search (fun xs => isAllEvensAccuOpt xs = some ())
 
-
-theorem foo : ∃ g : Nat → Bool,
-  f = (fun x b => x % 2 == 0 && b) →
-  -- (h : ∀ x acc, f x acc = (g x && acc)) →
-  (List.fold f true xs = true ↔
-  List.accuM
-    (fun _ _ => ())
-    (fun x _ _ => guard (g x))
-    (fun _ => some ())
-    xs
-    () = some () ) := by
-  refine Exists.intro ?g ?p
-  case p =>
-    intros hf
-    apply List.fold_accu_Option_true
-    aesop
-
-#print foo
-
 end AllEvensAccuOpt
