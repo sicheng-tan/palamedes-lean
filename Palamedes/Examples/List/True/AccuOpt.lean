9ca734cbfbf8dd679be24714335af55f10e9b84f
cassiatorczon <cassiatorczon@gmail.com>
2025-11-11 22:10:35 -0500

    removed unused var warning

--- Palamedes/Examples/List/True/AccuOpt.lean
+++ Palamedes/Examples/List/True/AccuOpt.lean
@@ -8,20 +8,10 @@ namespace TrueAccuOpt
 def isTrueAccuOpt (xs : List α) : Option Unit :=
   List.accuM
       (fun _ _ => ())
-      (fun x _ _ => guard true)
+      (fun _ _ _ => guard true)
       (fun _ => some ())
       xs
       ()
-
-/-
-(h : ∀ x acc, f x acc = (g x && acc)) :
-List.fold f true xs = true ↔
-    List.accuM
-      (fun _ _ => ())
-      (fun x _ _ => guard (g x))
-      (fun _ => some ())
-      xs
-      () = some ()-/
 
 def genTrueAccuOpt : Gen (List Nat) := by
   generator_search (fun xs => isTrueAccuOpt xs = some ())
